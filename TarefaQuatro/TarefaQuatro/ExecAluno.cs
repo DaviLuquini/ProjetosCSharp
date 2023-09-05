44a7aa785abf48b966cd830b21ed36adf6f2667a
Davi de Sá Luquini <davi262016@gmail.com>
2023-09-05 17:37:51 -0300

    Novo projeto utilizando classes e objetos em C#

---
+++ TarefaQuatro/TarefaQuatro/ExecAluno.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace TarefaQuatro {
+    internal class ExecAluno {
+        public static void ExecutarAluno() {
+            Aluno a1, a2;
+            a1 = new Aluno();
+            a2 = new Aluno();
+
+            Console.WriteLine("Digite seu nome:");
+            a1.nome = Console.ReadLine();
+            Console.WriteLine("Digite suas três notas:");
+            a1.nota1 = int.Parse(Console.ReadLine());
+            a1.nota2 = int.Parse(Console.ReadLine());
+            a1.nota3 = int.Parse(Console.ReadLine());
+
+            int notaFinal1 = a1.notaFinal(a1.nota1,a1.nota2,a1.nota3);
+
+            a1.aprovado(notaFinal1);
+            //Aluno 2
+            Console.WriteLine("Digite seu nome:");
+            a2.nome = Console.ReadLine();
+            Console.WriteLine("Digite suas três notas:");
+            a2.nota1 = int.Parse(Console.ReadLine());
+            a2.nota2 = int.Parse(Console.ReadLine());
+            a2.nota3 = int.Parse(Console.ReadLine());
+
+            int notaFinal2 = a2.notaFinal(a2.nota1, a2.nota2, a2.nota3);
+
+            a2.aprovado(notaFinal2);
+
+        }
+    }
+}
---
+++ TarefaQuatro/TarefaQuatro/ExecFunc.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace TarefaQuatro {
+    class ExecFunc {
+        public static void ExecutarFuncionario() {
+            Funcionario f1;
+            f1  = new Funcionario();
+            
+
+            Console.WriteLine("Digite seu nome:");
+            f1.nome = Console.ReadLine();
+            Console.WriteLine("Digite seu Salario Bruto:");
+            f1.salarioBruto = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite seu Imposto:");
+            f1.imposto = int.Parse(Console.ReadLine());
+
+            double salarioLiquido = f1.salarioLiquido(f1.salarioBruto, f1.imposto); 
+
+            string imprimir = f1.ToString();
+            Console.WriteLine(imprimir);
+
+            Console.WriteLine("Digite a porcentagem para aumentar o Salario:");
+            double porcentagem = int.Parse(Console.ReadLine());
+
+            double salarioAtualizado = f1.aumentarSalario(porcentagem, f1.salarioBruto, salarioLiquido);
+
+            Console.WriteLine($"Dados Atualizados " +
+                $"Funcionario: {f1.nome}, Salario ${salarioAtualizado}");
+        }
+    }
+}
