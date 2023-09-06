013a17548a9fb1b6e760c35f6d3a7552569886f2
Davi de Sá Luquini <davi262016@gmail.com>
2023-09-06 19:19:54 -0300

    TarefaTres

---
+++ ExecPessoa.cs
@@ -0,0 +1,46 @@
+﻿
+
+namespace TarefaTres {
+    class ExecPessoa {
+        /// <summary>
+        /// Esta Class pega o nome e a idade da pessoa1 e pessoa2 e verifica qual é a mais velha
+        /// </summary>
+        public static void ExecutarPessoa() {
+
+            Pessoa pessoa1, pessoa2;
+
+            pessoa1 = new Pessoa();
+            pessoa2 = new Pessoa();
+
+            Console.WriteLine("Escreva o nome da Primeira Pessoa");
+            pessoa1.nome = Console.ReadLine();
+            Console.WriteLine("Escreva a idade da Primeira Pessoa");
+            pessoa1.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Escreva o nome da Segunda Pessoa");
+            pessoa2.nome = Console.ReadLine();
+            Console.WriteLine("Escreva a idade da Segunda Pessoa");
+            pessoa2.idade = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Dados da primeira pessoa:");
+            Console.WriteLine("Nome: " + pessoa1.nome);
+            Console.WriteLine("Idade: " + pessoa1.idade);
+
+            Console.WriteLine("Dados da segunda pessoa:");
+            Console.WriteLine("Nome: " + pessoa2.nome);
+            Console.WriteLine("Idade: " + pessoa2.idade);
+
+            if (pessoa1.idade > pessoa2.idade) {
+                Console.WriteLine("Pessoa mais velha: " + pessoa1.nome);
+            }
+            else {
+                Console.WriteLine("Pessoa mais velha: " + pessoa2.nome);
+            }
+
+
+
+
+        }
+
+    }
+}
---
+++ Executor.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using TarefaTres;
+
+namespace TarefaTres {
+    class Executor {
+        /*static void Main(string[] args) {
+            Console.WriteLine("Qual projeto você quer testar?");
+            Console.WriteLine("Projeto das Pessoas digite (1) ");
+            Console.WriteLine("Projeto dos Funcionários digite (2) ");
+            int opcao = int.Parse(Console.ReadLine());
+
+            if (opcao == 1) {
+                ExecPessoa.ExecutarPessoa();
+            }
+            else if (opcao == 2) {
+                ExecFunc.ExecutarFuncionario();
+            }
+            else {
+                Console.WriteLine("Opção inválida");
+            }
+        }*/
+
+
+    }
+}
